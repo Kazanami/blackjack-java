2acbdbd96c9076e15c654ec21d8506ebb4691149
kazanami <6745680+Kazanami@users.noreply.github.com>
2019-06-01 11:55:39 +0900

    Initial Commit

---
+++ src/BlackJack.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Random;
+
+public class BlackJack {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //各種宣言
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int player_point = 0, enemy_point = 0;
+        Random rm = new Random();
+
+        int num = rm.nextInt(52);
+
+
+    }
+}
